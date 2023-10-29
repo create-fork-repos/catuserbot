6d69bfe9a67ecdd8e83d1e3329a4af3124b51b7e
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-10-29 10:07:49 +0530

    Update and rename exampleconfig.py to config.py

--- config.py
+++ config.py
@@ -4,17 +4,17 @@ from sample_config import Config
 class Development(Config):
     # get this values from the my.telegram.org
     APP_ID = 6
-    API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
+    API_HASH = "eb06d4abfb49dc3eeb1eb98ae0f581e"
     # the name to display in your alive message
-    ALIVE_NAME = "Your value"
+    ALIVE_NAME = "rootedcyber"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "Your value"
+    DB_URI = "postgres://uyhbesgl:lWqUutcVML9XiYODo1Ie_3LEnP67oePw@suleiman.db.elephantsql.com/uyhbesgl"
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "Your value"
+    STRING_SESSION = "Youralue"
     # create a new bot in @botfather and fill the following vales with bottoken
-    TG_BOT_TOKEN = "Your value"
+    TG_BOT_TOKEN = "1064540465:AAFTgEC_xtVSiu3RdoESorVfy_cAwZ5K0Qw"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
-    PRIVATE_GROUP_BOT_API_ID = -100
+    PRIVATE_GROUP_BOT_API_ID = -1001884618152
     # command handler
     COMMAND_HAND_LER = "."
     # command hanler for sudo
